683b01c5facd344c23aa087b158b212fdcc2cc79
Fateh Benmerzoug <fatehmtd@gmail.com>
2026-05-10 20:02:08 -0700

    feat: add constexpr catalog voice ID constants for all languages
    voice_constants.hpp: 237 catalog voices from docs.gradium.ai/guides/voices/all-voices
    organized into constexpr namespaces by language and dialect — no raw
    string literals needed in user code.
    
    Namespace hierarchy:
      gradium::voices::en::american   (71)  gradium::voices::en::british    (21)
      gradium::voices::en::australian  (6)  gradium::voices::en::other       (4)
      gradium::voices::fr::french     (40)  gradium::voices::fr::canadian    (3)
      gradium::voices::de             (50)
      gradium::voices::es::spanish    (11)  gradium::voices::es::mexican     (6)
      gradium::voices::es::other       (3)
      gradium::voices::pt::brazilian  (17)  gradium::voices::pt::portuguese  (7)
    
    Duplicate names within a dialect use a _2 suffix (fr::french::sarah_2,
    de::maximilian_2).

---
+++ lib/gradiumpp/include/gradiumpp/voice_constants.hpp
@@ -0,0 +1,333 @@
+#pragma once
+
+// All catalog voice IDs from https://docs.gradium.ai/guides/voices/all-voices
+// Usage: gradium::voices::<lang>::<dialect>::<name>
+
+namespace gradium::voices {
+
+// ── English ───────────────────────────────────────────────────────────────────
+
+namespace en {
+
+namespace american {
+
+constexpr const char* emma       = "YTpq7expH9539ERJ";
+constexpr const char* kent       = "LFZvm12tW_z0xfGo";
+constexpr const char* tiffany    = "Eu9iL_CYe8N-Gkx_";
+constexpr const char* christina  = "2H4HY2CBNyJHBCrP";
+constexpr const char* sydney     = "jtEKaLYNn6iif5PR";
+constexpr const char* john       = "KWJiFWu2O9nMPYcR";
+constexpr const char* maria      = "KNYHZTB8ZqdAZv0Q";
+constexpr const char* mark       = "dh0EzP6jCroK6prq";
+constexpr const char* logan      = "XJc-Y9tkSd1UA7s4";
+constexpr const char* juan       = "78zAgQK6xmExb8wS";
+constexpr const char* kaitlyn    = "56DcpvEI0Gawpidh";
+constexpr const char* mary       = "wPx6HPbUQkaUHGhq";
+constexpr const char* cameron    = "c8BzreHTk1GG2R4z";
+constexpr const char* jeremy     = "9QHzSiOYUD-RzEzM";
+constexpr const char* jesse      = "hOhCtzjR-cRG4T5T";
+constexpr const char* sean       = "cu0XE3Cxmg_GmSJ3";
+constexpr const char* olivia     = "kr-Om35JRqmA3Hzq";
+constexpr const char* shelby     = "O0uTTRx5zcetDFX4";
+constexpr const char* patrick    = "Z5GIOZR45ieZ8M-W";
+constexpr const char* richard    = "HndphaVV7KTCfKQT";
+constexpr const char* jason      = "FOFDH8py3aghc5kb";
+constexpr const char* timothy    = "v5lib8tjaosy5sxQ";
+constexpr const char* nathan     = "4NU5PqxX2BdMEtWe";
+constexpr const char* adam       = "EbIA5CIcQoa6NNd2";
+constexpr const char* abigail    = "KRo-uwfno-KcEgBM";
+constexpr const char* melissa    = "8Tm8RKFEbnkRtkdA";
+constexpr const char* allison    = "yU6yxQ3e8LKRwU84";
+constexpr const char* kelsey     = "MQC0U1yWvZXrppaF";
+constexpr const char* anna       = "PS7enm5lVZiIvEKV";
+constexpr const char* katherine  = "bvNlBZ3DWDoVy_Yc";
+constexpr const char* brian      = "ptMwY_gvmFxXMmDf";
+constexpr const char* jose       = "LqFNS0u6EII7VHBx";
+constexpr const char* dylan      = "d9Fl9x8luXXX7u6E";
+constexpr const char* rebecca    = "GJSxJhSTPAGIPDwy";
+constexpr const char* eric       = "knw-ddWDPNORRA4Z";
+constexpr const char* alyssa     = "22YWyuFACaMHsPh5";
+constexpr const char* alexandra  = "4nAcNUlNhEA_Kyjo";
+constexpr const char* jasmine    = "QPHuXnvRPQ57oXYy";
+constexpr const char* jordan     = "ws0Wb0PZXl21_Bbz";
+constexpr const char* cody       = "SqHUVuEiTPSlIB5r";
+constexpr const char* alex       = "91EdXxJDbWICDBgz";
+constexpr const char* brianna    = "fggSYM_FGJ30QTTl";
+constexpr const char* victoria   = "8dBmiTurwb7KcxLY";
+constexpr const char* nicole     = "T7UL6gmeDqqYiVe1";
+constexpr const char* jennifer   = "auZu0iT-fniQ4cJd";
+constexpr const char* stephanie  = "ikbJkd83GvuyoSLb";
+constexpr const char* kyle       = "CjQcj4yeIs6h0uAb";
+constexpr const char* lauren     = "SG3KnxbSOkkrY097";
+constexpr const char* alexis     = "74asmf7CXzjfopIX";
+constexpr const char* megan      = "exG4bLr-lZ_bI0jF";
+constexpr const char* jonathan   = "4u2uvwrHdTA2gRnZ";
+constexpr const char* robert     = "gTAO-3xLZ8_WSfbm";
+constexpr const char* alexander  = "8sWSyTC7byLsbHkr";
+constexpr const char* elizabeth  = "u8rA2xOF_0LRnNSb";
+constexpr const char* emily      = "i1kmq28cO60ia35K";
+constexpr const char* brandon    = "2j8TWGsIiUl4G3kj";
+constexpr const char* nicholas   = "n2Gv34jje2ZiiNzK";
+constexpr const char* ashley     = "QZMzHBlnJRjll_71";
+constexpr const char* joshua     = "bDlMqRew31ZJwrD-";
+constexpr const char* jessica    = "wYY8mXKrKtwKsaXZ";
+constexpr const char* jacob      = "ixaCTlZ5Xqf2XzQH";
+constexpr const char* matthew    = "X-wgJsZwQKhfebgK";
+constexpr const char* michael    = "Mj0Pzs94jCw8oVOC";
+constexpr const char* hannah     = "lP7D1y02OQFtffU3";
+constexpr const char* justin     = "6Mp6PGnaCdb-US21";
+constexpr const char* james      = "MZWrEHL2Fe_uc2Rv";
+constexpr const char* ryan       = "AqRuVz8-e8u3BR00";
+constexpr const char* taylor     = "EfuzJVuTmw_mA7PC";
+constexpr const char* sarah      = "aW5dxfdkzIFCIdXc";
+constexpr const char* samantha   = "mn5sS7D8kYKETZXA";
+constexpr const char* austin     = "-0MuXG9RcCsuSVtb";
+
+} // namespace american
+
+namespace british {
+
+constexpr const char* eva         = "ubuXFxVQwVYnZQhy";
+constexpr const char* jack        = "m86j6D7UZpGzHsNu";
+constexpr const char* kelly       = "Lxc7YlPC8ckLJA8H";
+constexpr const char* charles     = "P0GYBrxlhTy5CC87";
+constexpr const char* haley       = "aq7ltaIQ6ZJUY0jR";
+constexpr const char* steven      = "zyLIanWKViHkc6Wp";
+constexpr const char* madison     = "cuXxqSrGVntdhFpZ";
+constexpr const char* benjamin    = "IBVzgY91NZ1IJ0oP";
+constexpr const char* aaron       = "Ve1zknlflaRwcAQw";
+constexpr const char* christian   = "x69x43aS-5mVLCX2";
+constexpr const char* thomas      = "m7fJRmVaJjG2TL1c";
+constexpr const char* morgan      = "MGiwMOFxVe4a2aSU";
+constexpr const char* kimberly    = "Abqwk2RWxlBEyv0j";
+constexpr const char* courtney    = "UX3Hi2ZmK7tT0c3G";
+constexpr const char* amanda      = "ZZb4X9ueHSdRlv9q";
+constexpr const char* david       = "OceLYI_PPbqsdgdV";
+constexpr const char* rachel      = "dEcrv3B8XGHoox2_";
+constexpr const char* kayla       = "9VXl5t2IMagUQAzg";
+constexpr const char* tyler       = "Ow5IKhni2ED3Xxhl";
+constexpr const char* joseph      = "MhsYZQ4bIfcDpokF";
+constexpr const char* christopher = "fs2Qj_X2Z2WvWJSU";
+
+} // namespace british
+
+namespace australian {
+
+constexpr const char* hunter  = "W5htOuyiFI4Fwhxs";
+constexpr const char* samuel  = "pxKsJ_4kEMid5XpZ";
+constexpr const char* kevin   = "J2qsArcdozbto5Hn";
+constexpr const char* william = "VeVmpxxbyJiWrGNG";
+constexpr const char* anthony = "2V3TjbyQGPlkY6ON";
+constexpr const char* daniel  = "apU2CMobTyu92tZj";
+
+} // namespace australian
+
+namespace other {
+
+constexpr const char* max      = "NoJdNY6JTz-VJLwz";
+constexpr const char* arjun    = "-_aUUFZaJ0CT1gks";
+constexpr const char* michelle = "lt88kyLfD8Mqemla";
+constexpr const char* brittany = "3bIdO9CHnAh_pRAf";
+
+} // namespace other
+
+} // namespace en
+
+// ── French ────────────────────────────────────────────────────────────────────
+
+namespace fr {
+
+namespace french {
+
+constexpr const char* elise     = "b35yykvVppLXyw_l";
+constexpr const char* leo       = "axlOaUiFyOZhy4nv";
+constexpr const char* olivier   = "vMYQUSzm6GRkJX6d";
+constexpr const char* manon     = "p1fSBpcmVWngBqVd";
+constexpr const char* jade      = "3mM3xaoFjNMQa22C";
+constexpr const char* amelie    = "J4XbCGPYNMigXcfZ";
+constexpr const char* adrien    = "0LMAi0x_YVG_GLeM";
+constexpr const char* sarah     = "-dOnYAX4N4GqSOee";
+constexpr const char* jennifer  = "N8xxxD_d-ZinGVI4";
+constexpr const char* elodie    = "zba0owtqy4Gnewn9";
+constexpr const char* justine   = "TJv-kucMsUo24VQe";
+constexpr const char* oceane    = "YE0-JPiElafJrZaC";
+constexpr const char* lea       = "QY_BJKHMElKDO12-";
+constexpr const char* sarah_2   = "QkmUhBH4hIV2_BkY";
+constexpr const char* mathieu   = "D-IpHY1UI0iX9xQD";
+constexpr const char* clement   = "twLGV8mrH_ycNpUn";
+constexpr const char* julie     = "k1wgs3k8-wRxTJO6";
+constexpr const char* dylan     = "Hdf5cdfaGrLDTD63";
+constexpr const char* marion    = "1VAVLmmbQFDw7TMn";
+constexpr const char* pauline   = "2AtP1urAQkZaeI2U";
+constexpr const char* vincent   = "B09t5S64xLaKwXeW";
+constexpr const char* pierre    = "AroCL6f1qizjiZ_a";
+constexpr const char* guillaume = "qTA0lxFpynJdoxx7";
+constexpr const char* romain    = "zpmn3GOfiU_i5QGo";
+constexpr const char* kevin     = "IB53xJtufx1sbfbt";
+constexpr const char* florian   = "kw_VWSocR7vyA9Ty";
+constexpr const char* antoine   = "hx1RAC4Lqd9xyTAr";
+constexpr const char* quentin   = "pdcyd1mLmo0fcg3O";
+constexpr const char* adam      = "aNiSRZ0BhQxO1FPx";
+constexpr const char* anais     = "ImBVnxSeLsdCfNIV";
+constexpr const char* marine    = "GmGF_3ETsY2Zq7_w";
+constexpr const char* camille   = "w9V1722uEmTkWqnR";
+constexpr const char* marie     = "BbLb4TxdlrldgpHI";
+constexpr const char* thomas    = "8nsAoui8Y5RK9PYw";
+constexpr const char* chloe     = "rIYDMY3dLccdauWA";
+constexpr const char* nicolas   = "mxcKXLymdLQCdlEq";
+constexpr const char* laura     = "Jlh1B0PKQJyup0sQ";
+constexpr const char* amandine  = "NvHEAMGiPT4u8iT-";
+constexpr const char* valentin  = "WWHSNJCSTm77dyGd";
+constexpr const char* manu      = "L6OaiBybqikfCBk0";
+
+} // namespace french
+
+namespace canadian {
+
+constexpr const char* melanie   = "xynYWquoAsrvM7UY";
+constexpr const char* maxime    = "s0PhgjzOTRD5wo5L";
+constexpr const char* alexandre = "HBfu9XA3QfzAG1MN";
+
+} // namespace canadian
+
+} // namespace fr
+
+// ── German ────────────────────────────────────────────────────────────────────
+
+namespace de {
+
+constexpr const char* mia           = "-uP9MuGtBqAvEyxI";
+constexpr const char* maximilian    = "0y1VZjPabOBU3rWy";
+constexpr const char* moritz        = "IIZIkBSZAmb9nFZb";
+constexpr const char* lisa          = "kAoOc9Yb5EQDzA-N";
+constexpr const char* hans          = "vbg20SqFS_gBntTQ";
+constexpr const char* franziska     = "VXA4-0_ZN4o8q3vK";
+constexpr const char* david         = "zyla-_bhVQtNTBdT";
+constexpr const char* lea           = "lSVEPWl_N_7MtcHe";
+constexpr const char* stefanie      = "hXjVvZ6oDDGQAQFj";
+constexpr const char* tom           = "xq0vDziADfAmg6Uh";
+constexpr const char* niklas        = "-qKylkN2UPxd7Mmg";
+constexpr const char* michelle      = "fJDF4lEH590XplFv";
+constexpr const char* jasmin        = "h2o5CDDhV5wE3Bwi";
+constexpr const char* dominik       = "ZOiGbnYdgKSBM_rH";
+constexpr const char* sabrina       = "dK5Glio51HTxdMu0";
+constexpr const char* dennis        = "YHkMHL6WppbXd42a";
+constexpr const char* julian        = "LAmPTQZkwYJKRCKt";
+constexpr const char* jannik        = "RPw-aWdY8NBiIWeg";
+constexpr const char* melanie       = "bauuigqCZbJFfk5q";
+constexpr const char* christian     = "WxHB2b5HxA0Kuq5u";
+constexpr const char* nadine        = "9O8ZawShJ7UwURjK";
+constexpr const char* nicole        = "t1Y_yKjku5R46F9t";
+constexpr const char* sebastian     = "KEMqb7dQlTCAEUx6";
+constexpr const char* lena          = "df4Al5gt14Am4Qaf";
+constexpr const char* fabian        = "42-EbMFThYfhVB83";
+constexpr const char* patrick       = "3-pqEMoGtIq7wXtH";
+constexpr const char* christina     = "9LhjfdN9LOrygqDi";
+constexpr const char* jessica       = "--9DFXOPx8kJFsbe";
+constexpr const char* jennifer      = "XFttJvHwReWtWQNQ";
+constexpr const char* vanessa       = "8eZwfGLoSF2N0RB3";
+constexpr const char* maria         = "sz-H9BxaRaqxQ2S0";
+constexpr const char* jonas         = "6tFmjkrmrdhO2bXV";
+constexpr const char* anna          = "D8iRHK1qJhqfE00v";
+constexpr const char* marcel        = "Cw79FL0p0J6UM9El";
+constexpr const char* kevin         = "AySdCEnP2nqRo1WM";
+constexpr const char* tobias        = "uycTGmIXbw_Y83p9";
+constexpr const char* daniel        = "H0GE4TqfCQGmpQhL";
+constexpr const char* tim           = "-WFy9WtlQNE-dEV2";
+constexpr const char* philipp       = "ZsVFAOnjnEPxJVDI";
+constexpr const char* maximilian_2  = "H3Rh9kJcd4gZidvN";
+constexpr const char* sarah         = "ApPgTz3nMHOsWxhK";
+constexpr const char* florian       = "XnSnbQW98he4aULg";
+constexpr const char* katharina     = "AEJ61XaIaRill4cJ";
+constexpr const char* mona          = "T2NDxsof9FHYxgJj";
+constexpr const char* laura         = "wBgI9XmASQwvQ13w";
+constexpr const char* felix         = "uF8PfAXrv6qU9UEM";
+constexpr const char* julia         = "FRTqjB2TL-Ix9GXW";
+constexpr const char* alexander     = "xki1DK6Ks6tuDmcb";
+constexpr const char* lukas         = "5UkFVe2B8OqLo-5R";
+constexpr const char* jan           = "1D38wv1wp-H7QcyM";
+
+} // namespace de
+
+// ── Spanish ───────────────────────────────────────────────────────────────────
+
+namespace es {
+
+namespace spanish {
+
+constexpr const char* sergio    = "xu7iJ_fn2ElcWp2s";
+constexpr const char* sofia     = "s4CzgVHP5cEkB9LD";
+constexpr const char* pablo     = "aCWBiYUiQ4VwW8_b";
+constexpr const char* carlos    = "yPxeHKlCzaHeKd_V";
+constexpr const char* alberto   = "h39kz1iyoymcjcqh";
+constexpr const char* elena     = "PqjKPYFyGNsg1YU-";
+constexpr const char* ana       = "ynR4CAbXMiOv-vGC";
+constexpr const char* sara      = "lPCVUcicz2XRaLE3";
+constexpr const char* daniel    = "R3L8t75ZEoZCPUA9";
+constexpr const char* alejandro = "eorxD0DWv--n7l3p";
+constexpr const char* miguel    = "Gijj_GPBfJVcP-FZ";
+
+} // namespace spanish
+
+namespace mexican {
+
+constexpr const char* valentina = "B36pbz5_UoWn4BDl";
+constexpr const char* adrian    = "r5WB0b126tlHSrku";
+constexpr const char* sergio    = "-8ZoUJpVU98rxpv9";
+constexpr const char* david     = "zdE2H9vw2vcMl_Pt";
+constexpr const char* marta     = "VAb2M8nKHlUUZBk4";
+constexpr const char* cristina  = "Bwl2KLUPxf82_ZaJ";
+
+} // namespace mexican
+
+namespace other {
+
+constexpr const char* javier = "wGhY_zZCoQ5gB0ce";
+constexpr const char* carmen = "zhH3lPUo-JxmlOJT";
+constexpr const char* maria  = "k2B3TJiffePxjeBn";
+
+} // namespace other
+
+} // namespace es
+
+// ── Portuguese ────────────────────────────────────────────────────────────────
+
+namespace pt {
+
+namespace brazilian {
+
+constexpr const char* alice     = "pYcGZz9VOo4n2ynh";
+constexpr const char* davi      = "M-FvVo9c-jGR4PgP";
+constexpr const char* laura     = "xB86uC_i8sO2U41-";
+constexpr const char* frederico = "L7890s1B44FqSiGC";
+constexpr const char* eduardo   = "hAdJ9w9xBQkFgrRl";
+constexpr const char* daniel    = "_cP-0vSYfMmzR4al";
+constexpr const char* thiago    = "QZtWUy8jmIroWiOu";
+constexpr const char* pedro     = "Yee42wDKxEFHi0BS";
+constexpr const char* jessica   = "Fmt16x6anKfMMeSx";
+constexpr const char* fernando  = "8QUaJGjSFdgHkuI8";
+constexpr const char* ana       = "24cfpJbYGXZLE39T";
+constexpr const char* gustavo   = "T4yRIRCLji61Fz-N";
+constexpr const char* bruno     = "isyT17KHEj84P9w9";
+constexpr const char* leticia   = "h6qFHXR3-bqPg_PE";
+constexpr const char* gabriel   = "4ubKCfFxLeBg-cbl";
+constexpr const char* lucas     = "AaTW_13X1yYe_OnX";
+constexpr const char* joao      = "YHOBjtajNBEHUI_K";
+
+} // namespace brazilian
+
+namespace portuguese {
+
+constexpr const char* rodrigo   = "EzmLkNorEpZG_oNv";
+constexpr const char* bruna     = "Du_Dcv4fgXBDdubR";
+constexpr const char* leonardo  = "YUKEEk7Y4Igsj1Ts";
+constexpr const char* matheus   = "wT1bHy1Vq_0Bn73I";
+constexpr const char* juliana   = "B6aHVROMF8FuKR07";
+constexpr const char* maria     = "73lMH7Zcc411nxJz";
+constexpr const char* rafael    = "KpDAXeGeen7P9Uri";
+
+} // namespace portuguese
+
+} // namespace pt
+
+} // namespace gradium::voices
